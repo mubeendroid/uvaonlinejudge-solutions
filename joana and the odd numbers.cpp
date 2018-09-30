7bfaf1c397bb26131ba6179cc20f20a1cdf4048a
Mubeen Ul Alam <mubeendroid@gmail.com>
2018-09-30 13:40:49 +0600

    UVA problem number 913 solve

---
+++ joana and the odd numbers.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    long long int a,n;
+    while(cin>>n)
+    {
+        a=(((n*(n+2)/2)*3)-6);
+        cout<<a<<endl;
+    }
+    return 0;
+}
